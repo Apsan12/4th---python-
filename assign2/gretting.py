fbb70462fd07a994037416e1f9583b7b9ed523f6
Apsan12 <apsanneupane345@gmail.com>
2025-11-09 07:46:24 +0545

    5th asign

--- assign2/gretting.py
+++
@@ -1,5 +0,0 @@
-def make_card(name, age, message="Have a good day."):
-    return f"Hello {name} ({age} years old)!\n{message}"
-user_name = input("Enter your name: ")
-user_age = int(input("Enter your age: "))
-print(make_card(user_name, user_age))
